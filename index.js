1cfaa31e54985130a9e1eecfd176d21477fcdac2
Johnathan Howard <johnathan.howardjr@gmail.com>
2018-01-22 18:56:23 -0500

    updated function spelling

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = Object.assign({},
     require('./lib/isEqual.js'),
     require('./lib/isEven.js'),
     require('./lib/isOdd.js'),
-    require('./lib/isPostiveNumber.js')
+    require('./lib/isPositiveNumber.js')
 );
