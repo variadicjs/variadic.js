47e62875f64aab56105903d05a2b6a7dff2a2e0b
Jeff Bothe <jeffrymbothe@gmail.com>
2018-01-24 15:37:36 -0500

    improve comments and test statements

--- spec/isPrimeSpec.js
+++ spec/isPrimeSpec.js
@@ -1,22 +1,22 @@
 describe('IsPrime', () => {
   const { isPrime } = require('../lib/isPrime');
 
-  it('should return true when all parameters are prime numbers', () => {
-    const result = isPrime(2, 3, 5);
+  it('should return true when all parameters evaluate to prime numbers', () => {
+    const result = isPrime(2, 3, 10 / 2, +'7');
     expect(result).toBe(true);
   });
 
-  it('should return false when at least one parameter is not a prime number', () => {
+  it('should return false when at least one parameter does not evaluate to a prime number', () => {
     const result = isPrime(2, 3, 4, 5);
     expect(result).toBe(false);
   });
 
-  it('should return false when at least one parameter is not an integer', () => {
+  it('should return false when at least one parameter does not evaluate to an integer', () => {
     const result = isPrime(2.5, 3, 4, 5);
     expect(result).toBe(false);
   });
 
-  it('should return false when at least one parameter is less than 2', () => {
+  it('should return false when at least one parameter evaluates to a number less than 2', () => {
     const result = isPrime(-2, 3, 4, 5);
     expect(result).toBe(false);
   });
@@ -25,11 +25,11 @@ describe('IsPrime', () => {
     expect(isPrime).toThrow();
   });
 
-  it('should throw an error when at least one parameter is not a number', () => {
+  it('should throw an error when at least one parameter does not evaluate to a number', () => {
     expect(() => isPrime('yo')).toThrow();
   });
 
-  it('should throw an error when at least one parameter is a number larger than Number.MAX_SAFE_INTEGER', () => {
+  it('should throw an error when at least one parameter evaluates to a number larger than Number.MAX_SAFE_INTEGER', () => {
     expect(() => isPrime(9949370777987917 )).toThrow();
   });
 });
