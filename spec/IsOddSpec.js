8285d7655e9aab0bd36528f638a64347661b9441
Johnathan Howard <johnathan.howardjr@gmail.com>
2018-01-25 18:14:39 -0500

    addressing lint fndings and fixing test

--- spec/IsOddSpec.js
+++ spec/IsOddSpec.js
@@ -1,43 +1,43 @@
+const { isOdd } = require('../lib/isOdd');
+
 describe('IsOdd', () => {
-    const { isOdd } = require('../lib/isOdd');
+  it('should return false when all parameters are even numbers', () => {
+    const result = isOdd(2, 4, 200);
+    expect(result).toBe(false);
+  });
 
-    it('should return false when all parameters are even numbers', () => {
-        const result = isOdd(2, 4, 200);
-        expect(result).toBe(false);
-    });
+  it('should return true when all parameters are odd numbers', () => {
+    const result = isOdd(1, 3, 1);
+    expect(result).toBe(true);
+  });
 
-    it('should return true when all parameters are odd numbers', () => {
-        const result = isOdd(1, 3, 1);
-        expect(result).toBe(true);
-    });
+  it('should return true when all parameters are odd and floating point numbers', () => {
+    const result = isOdd(1, 3, 201.21);
+    expect(result).toBe(true);
+  });
 
-    it('should return true when all parameters are odd and floating point numbers', () => {
-        const result = isOdd(1, 3, 201.21);
-        expect(result).toBe(true);
-    });
+  it('should return false when a string with an even number is passed', () => {
+    const result = isOdd(1, 3, '8');
+    expect(result).toBe(false);
+  });
 
-    it('should return false when a string with an even number is passed', () => {
-        const result = isOdd(1, 3, '8');
-        expect(result).toBe(false);
-    });
+  it('should return true when a string with an odd number is passed', () => {
+    const result = isOdd(1, 3, '5');
+    expect(result).toBe(true);
+  });
 
-    it('should return true when a string with an odd number is passed', () => {
-        const result = isOdd(1, 3, '5');
-        expect(result).toBe(true);
-    });
+  it('should return false when an object is passed', () => {
+    const result = isOdd(1, 3, {});
+    expect(result).toBe(false);
+  });
 
-    it('should return false when an object is passed', () => {
-        const result = isOdd(1, 3, {});
-        expect(result).toBe(false);
-    });
+  it('should return false when an array is passed', () => {
+    const result = isOdd(1, 3, []);
+    expect(result).toBe(false);
+  });
 
-    it('should return false when an array is passed', () => {
-        const result = isOdd(1, 3, []);
-        expect(result).toBe(false);
-    });
-
-    it('should return false when a boolean is passed', () => {
-        const result = isOdd(1, 3, true);
-        expect(result).toBe(false);
-    });
+  it('should return false when a boolean is passed', () => {
+    const result = isOdd(1, 3, true);
+    expect(result).toBe(false);
+  });
 });
