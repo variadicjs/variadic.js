9f3abd4d55dc938015a46d6289d698b1cebc62e7
jmbothe <jeffrymbothe@gmail.com>
2018-01-29 19:59:05 -0500

    re-work mode function output

--- spec/modeSpec.js
+++ spec/modeSpec.js
@@ -1,39 +1,32 @@
 const { mode } = require('../lib/mode');
 
 describe('mode', () => {
-  it('should return the value and frequecny of the mode of all parameters, given that all parameters evaluate to numbers', () => {
+  it('should return the mode of all parameters, given that all parameters evaluate to numbers', () => {
     const result = mode(1, 2, 9 / 3, 3, +'4', 5e+0);
-    expect(result).toEqual([{ value: 3, frequency: 2 }]);
+    expect(result).toEqual([3]);
   });
 
-  it('should return the value and frequency of the sole number if the set is of length 1', () => {
+  it('should return the value of the sole number if the set is of length 1', () => {
     const result = mode(1);
-    expect(result).toEqual([{ value: 1, frequency: 1 }]);
+    expect(result).toEqual([1]);
   });
 
-  it('should return the value and frequency of the sole number if all the numbes in the set are the same', () => {
+  it('should return the value of the sole number if all the numbes in the set are the same', () => {
     const result = mode(1, 2 / 2, +'1', 1e+0);
-    expect(result).toEqual([{ value: 1, frequency: 4 }]);
+    expect(result).toEqual([1]);
   });
 
-  it('should return an array of values/frequencies if the set is bimodal or multimodal', () => {
+  it('should return an array of values if the set is bimodal or multimodal', () => {
     let result = mode(1, 1, 2, 2, 3);
-    expect(result).toEqual([
-      { value: 1, frequency: 2 },
-      { value: 2, frequency: 2 }
-    ]);
+    expect(result).toEqual([1, 2]);
 
-    result = mode(1, 1, 2, 2, 3, 3, 4);
-    expect(result).toEqual([
-      { value: 1, frequency: 2 },
-      { value: 2, frequency: 2 },
-      { value: 3, frequency: 2 }
-    ]);
+    result = mode(1, 2, 2, 1, 4, 3, 3, 2, 1, 3);
+    expect(result).toEqual([1, 2, 3]);
   });
 
-  it('should return a string "set has no mode", if the set has no mode', () => {
+  it('should return "undefined" if the set has no mode', () => {
     const result = mode(1, 2, 3, 4, 5);
-    expect(result).toBe('set has no mode');
+    expect(result).toBe(undefined);
   });
 
   it('should throw an error when no parameters are provided', () => {
