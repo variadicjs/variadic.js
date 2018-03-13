19a851e238d69ddf141f66ab428ca90300eb1e95
someyoungideas <someyoungideas@gmail.com>
2018-03-12 21:18:25 -0500

    updating to isIntegers in isIntegers spec

--- spec/isIntegers.js
+++ spec/isIntegers.js
@@ -2,31 +2,31 @@ const { isIntegers } = require('../lib/isIntegers');
 
 describe('isIntegers', () => {
   it('should throw an error if no parameters are provided', () => {
-    expect(() => isInteger()).toThrow();
+    expect(() => isIntegers()).toThrow();
   });
 
   it('should throw an error if any parameters provided are not numbers', () => {
-    expect(() => isInteger('')).toThrow();
-    expect(() => isInteger('test', 2)).toThrow();
+    expect(() => isIntegers('')).toThrow();
+    expect(() => isIntegers('test', 2)).toThrow();
   });
 
   it('should return false if any parameters are not an integer', () => {
-    expect(isInteger(2.5)).toBe(false);
-    expect(isInteger(2, 2.5)).toBe(false);
+    expect(isIntegers(2.5)).toBe(false);
+    expect(isIntegers(2, 2.5)).toBe(false);
   });
 
   it('should return true if all parameters are integers', () => {
-    expect(isInteger(0)).toBe(true);
-    expect(isInteger(2)).toBe(true);
-    expect(isInteger(-1)).toBe(true);
-    expect(isInteger(-1, 0, 2)).toBe(true);
+    expect(isIntegers(0)).toBe(true);
+    expect(isIntegers(2)).toBe(true);
+    expect(isIntegers(-1)).toBe(true);
+    expect(isIntegers(-1, 0, 2)).toBe(true);
   });
 
   it('should return true if all parameters can parse to integer', () => {
-    expect(isInteger('0')).toBe(true);
-    expect(isInteger('2')).toBe(true);
-    expect(isInteger('-1')).toBe(true);
-    expect(isInteger('-1', '0', '2')).toBe(true);
-    expect(isInteger('-1', 0, '2')).toBe(true);
+    expect(isIntegers('0')).toBe(true);
+    expect(isIntegers('2')).toBe(true);
+    expect(isIntegers('-1')).toBe(true);
+    expect(isIntegers('-1', '0', '2')).toBe(true);
+    expect(isIntegers('-1', 0, '2')).toBe(true);
   });
 });
